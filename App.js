3ce6c14dd2628a68eb1ed81624b3ab7e4a10bc32
aivelabs <tkdlxm457@naver.com>
2022-01-24 09:56:46 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react"
+import * as Location from 'expo-location';
+import { StyleSheet, Text, Dimensions, View, ScrollView, ActivityIndicator } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+const API_KEY = "1df0128bfaaeb29fd5bfdaa2aa62f8d0";
 
 export default function App() {
+
+  const [city, setCity] = useState("Loading...");
+
+  const [days, setDays] = useState([]);
+
+  const [ok, setOk] = useState(true);
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].district);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`)
+    const json = await response.json();
+    // setDays(json.daily);
+  }
+
+  useEffect(() => {
+    getWeather();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}>
+        {
+          days.length === 0 ? (
+            <View style={styles.day}>
+              <ActivityIndicator color="white" size="large"/>
+            </View>
+          ) : (
+            <View style={styles.day}>
+
+            </View>
+          )}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +62,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "yellow",
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 60,
+    fontWeight: "500",
+  },
+  weather: {
+
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  teamp: {
+    marginTop: 50,
+    fontSize: 180,
+  },
+  desc: {
+    marginTop: -30,
+    fontSize: 50,
   },
 });
