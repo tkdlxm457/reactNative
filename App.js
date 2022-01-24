1d1784f7f08aedb5c5c25a6241e87188e46168f4
aivelabs <tkdlxm457@naver.com>
2022-01-24 14:34:34 +0900

    아이콘 추가

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from "react"
 import * as Location from 'expo-location';
 import { StyleSheet, Text, Dimensions, View, ScrollView, ActivityIndicator } from 'react-native';
+import { Fontisto } from '@expo/vector-icons';
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 const API_KEY = "1df0128bfaaeb29fd5bfdaa2aa62f8d0";
+
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atomosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+
+}
 
 export default function App() {
 
@@ -25,9 +37,9 @@ export default function App() {
       { useGoogleMaps: false }
     );
     setCity(location[0].district);
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`)
     const json = await response.json();
-    // setDays(json.daily);
+    setDays(json.daily);
   }
 
   useEffect(() => {
@@ -47,12 +59,22 @@ export default function App() {
         {
           days.length === 0 ? (
             <View style={styles.day}>
-              <ActivityIndicator color="white" size="large"/>
+              <ActivityIndicator style={{...styles.days, alignItems: "center"}}/>
             </View>
           ) : (
-            <View style={styles.day}>
+            days.map((day, index) =>
+              <View style={styles.day}>
+                <View style={{flexDirection: "row", alignItems: "center", width: "100%", justifyContent: "space-between"}}>
+                  <Text style={styles.teamp}>
+                    {parseFloat(day.temp.day).toFixed(1)}°
+                  </Text>
+                  <Fontisto name={icons[day.weather[0].main]} size={68} color="black" />
+                </View>
 
-            </View>
+                <Text style={styles.desc}>{day.weather[0].main}</Text>
+                <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+              </View>
+            )
           )}
       </ScrollView>
     </View>
@@ -78,14 +100,18 @@ const styles = StyleSheet.create({
   },
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "flex-start",
+    padding: 15,
   },
   teamp: {
     marginTop: 50,
-    fontSize: 180,
+    fontSize: 120,
   },
   desc: {
     marginTop: -30,
-    fontSize: 50,
+    fontSize: 30,
+  },
+  tinyText: {
+    fontSize: 20,
   },
 });
